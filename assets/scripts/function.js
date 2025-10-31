0bfc49ea3633daf0372c6cd67f4c0654541ac3e2
Nilton Miguel <niltonmiguel0@gmail.com>
2025-10-30 21:13:53 -0300

    primeiro projeto criado

--- assets/scripts/function.js
+++ assets/scripts/function.js
@@ -1,8 +1,21 @@
-const btn = document.getElementById('cadastrar');
-btn.addEventListener('click',function(e){
-    e.preventDefault();
-
-    const cepInput= document.getElementById('cep').value;
-    const cep = cepInput.value;
-    console.log(cep);
+document.getElementById('cadastrar').addEventListener('click' , function(e){
+   e.preventDefault();
+   const cep = document.getElementById('cep').value.replace("-","");
+   if(cep =="" || cep.length != 8){
+        alert("digite um cep válido");
+   }
+   const url = `https://viacep.com.br/ws/${cep}/json/`;
+   fetch(url).then(resposta =>{
+        if(! resposta){
+            alert('erro na sua consulta do cep');
+        }
+        return resposta.json();
+   }).then(dados =>{
+        document.getElementById('cidade').value = dados.localidade;
+        document.getElementById('bairro').value = dados.bairro;
+        document.getElementById('rua').value = dados.logradouro;
+        document.getElementById('estado').value = dados.estado;
+   })
 })
+    
+    
